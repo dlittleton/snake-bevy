9b0a5dba4a28a19f68c89e7296c153f05d9b62a2
Duane Littleton <dlittleton87@gmail.com>
2025-03-29 11:56:17 -0400

    Add resource timer to control print frequency

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,14 @@ use bevy::{
     },
 };
 
-fn hello_world() {
-    println!("hello world!");
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(HelloTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, greet_people);
+    }
 }
 
 #[derive(Component)]
@@ -16,15 +22,20 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct HelloTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("foo".to_string())));
     commands.spawn((Person, Name("bar".to_string())));
     commands.spawn((Person, Name("baz".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<HelloTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -37,7 +48,6 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins(HelloPlugin)
         .run();
 }
