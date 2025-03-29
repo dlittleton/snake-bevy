30793e075f43a16ed7ae8aa8a38502c062aaa3ec
Duane Littleton <dlittleton87@gmail.com>
2025-03-29 11:47:24 -0400

    Add basic system following quickstart guide
    Currently spamming render errors to the console

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,28 @@ fn hello_world() {
     println!("hello world!");
 }
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("foo".to_string())));
+    commands.spawn((Person, Name("bar".to_string())));
+    commands.spawn((Person, Name("baz".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
